ac594dad0d672e4441aa04d666774b9cb27771fd
Ankit Kumar Singh <107400903+ankit-t12@users.noreply.github.com>
2024-10-15 09:48:50 +0530

    15oct

--- src/Oct/ex_08_10_2024/Lab001.java
+++ src/Oct/ex_08_10_2024/Lab001.java
@@ -1,4 +1,7 @@
 package Oct.ex_08_10_2024;
 
 public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
---
+++ src/Oct/ex_08_10_2024/Lab002.java
@@ -0,0 +1,18 @@
+package Oct.ex_08_10_2024;
+import java.util.Scanner;
+
+public class Lab002
+{
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("The sum is " +c);
+
+    }
+}
